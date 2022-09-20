ec72835064f083f94d886a1e39b596ea74ccee0a
jakenotsnake <106646682+jakenotsnake@users.noreply.github.com>
2022-09-20 17:30:44 +0930

    tbh not a lot of progress

--- philo/srcs/init.c
+++ philo/srcs/init.c
@@ -6,21 +6,31 @@
 /*   By: jtanner <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 15:46:48 by jtanner           #+#    #+#             */
-/*   Updated: 2022/09/19 19:42:50 by jtanner          ###   ########.fr       */
+/*   Updated: 2022/09/20 17:30:05 by jtanner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void	initdudes(int n, int d, int e, int s, int o)
+void	initdudes(int np, int d, int e, int s, int o)
 {
 	t_dudes	*p;
 
 	p = (t_dudes *)malloc(sizeof (t_dudes));
-	p->no = n;
+	p->id = pthread_self();
+	p->no = np;
 	p->hunger = d;
 	p->time_eat = e;
 	p->time_sleep = s;
 	p->eaten = o;
-	pthread_create(&p->id, NULL, startdudes(p), NULL);
+	printf("no:%d, hunger:%d, eat:%d, sleep:%d, eaten:%d\n", p->no, p->hunger, p->time_eat, p->time_sleep, p->eaten);
+	startdudes(p);
+}
+
+void	startthread(int n, int d, int e, int s, int o)
+{
+	pthread_t	t[n];
+	
+	while (n-- >=2)
+		pthread_create(&t[n], NULL, &initdudes(n, d, e, s, o), NULL);
 }
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,14 +6,14 @@
 /*   By: jtanner <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 16:47:49 by jtanner           #+#    #+#             */
-/*   Updated: 2022/09/19 19:43:43 by jtanner          ###   ########.fr       */
+/*   Updated: 2022/09/20 17:25:35 by jtanner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/philo.h"
 
 int	main(int ac, char **av)
 {
-	int i[5];
+	int i[5];;
 	int	c;
 
 	if (ac == 6)
@@ -22,16 +22,16 @@ int	main(int ac, char **av)
 		while (c++ < 4)
 			i[c] = atoi(av[c + 1]);
 		i[0] += 1;
-		while (i[0]-- >= 2)
-			initdudes(i[0], i[1], i[2], i[3], i[4]);
+		startthread(i[0], i[1], i[2], i[3], i[4]);
 	}
 	return(0);
 	exit(0);
 }
 
-void	*startdudes(t_dudes *p)
+void	startdudes(t_dudes *p)
 {
 	gettimeofday(&p->time, NULL);
 	printf("%d: %d Has awoken\n", p->time.tv_usec, p->no);
-	return(0);
+	sleep(3);
+	printf("hello\n");
 }
